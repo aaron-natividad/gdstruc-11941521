26500edba2e9f4189103ba3e974f96df301474da
Justin Aaron Natividad <justinaaron.natividad@benilde.edu.ph>
2021-08-30 15:11:49 +0800

    Added Module 7 and Answer to Quiz 6

---
+++ gdstruc-module7/src/com/aaronnatividad/Tree.java
@@ -0,0 +1,27 @@
+package com.aaronnatividad;
+
+public class Tree {
+    private Node root;
+
+    public void insert(int value){
+        if(root == null){
+            root = new Node(value);
+        }
+        else {
+            root.insert(value);
+        }
+    }
+
+    public void traverseInOrder(){
+        if(root != null){
+            root.traverseInOrder();
+        }
+    }
+
+    public Node get(int value){
+        if(root != null){
+            return root.get(value);
+        }
+        return null;
+    }
+}
